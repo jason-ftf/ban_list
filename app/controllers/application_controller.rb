2dea6f3b2f1482d5c5607892ed1ef3e88e44b629
Jason Chee <101130629+jason-ftf@users.noreply.github.com>
2022-08-04 12:45:22 -0400

    initial commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
-  # def after_sign_in_path_for(resource)
-  #   home_path
-  # end
+  def after_sign_in_path_for(resource)
+    home_path
+  end
 
   def remote_ip
     if request.remote_ip == '::1'
